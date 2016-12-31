cab24e1c54113868c7deaf94e09d4db4563ad921
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-12-31 12:44:22 +0100

    Add logfile, reduce echo and manage motion detection
    Add 'comma-separated values' logfile
    Reduce result on shell (redirection to logfile)
    Add the hability to switch on/off motion detection on DCS camera

--- domotic/bluetooth-presence.sh
+++ domotic/bluetooth-presence.sh
@@ -2,14 +2,18 @@
 #Goal: Detect presence thanks to à Bluetooth device (cellphone, smartwatch, beacon, headset,...).
 #Source: http://clement.storck.me/blog/2016/02/detection-de-presence-en-bluetooth/
 
-COUNTPRESENT=0
-COUNTABSENT=0
-OWNER=0600000000 #Cellphone number
+COUNTPRESENT='0'
+COUNTABSENT='0'
+DATESPECIFIC=$(date "+%d/%m/%Y,%H:%M:%S")
+LOGFILE='/home/pi/domotic/bluetooth-presence.sh.log'
+OWNER='06xxxxxxxx' #Cellphone number
+CAMIP='xxx.xxx.xxx.xxx'
+CAMAUTHCODE=''
 
 while true
 do
     DEVICE=`hcitool name $1`
-    
+
     if [[ $DEVICE = $2 ]]
     then
         COUNTPRESENT=$((COUNTPRESENT+1))
@@ -20,8 +24,11 @@ do
         then
                 # Do something here
                 COUNTABSENT=0
-                echo "$(date) - "$2" is present"
-                /home/pi/jarvis/jarvis.sh -s "Welcome $2" #Ask Jarvis (domotic assistant) to welcome the user vocally
+                #echo "$(date) - "$2" is present"
+                echo $DATESPECIFIC","$2",present" >> $LOGFILE
+                #/home/pi/jarvis/jarvis.sh -s "Welcome $2" #Ask Jarvis (domotic assistant) to welcome the user vocally
+                curl "http://$CAMIP/setSystemMotion" -H "Host: $CAMIP" -H "Referer: http://$CAMIP/setSystemMotion" -H "Authorization: Basic $CAMAUTHCODE" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" --data "ReplySuccessPage=motion.htm&ReplyErrorPage=motion.htm&MotionDetectionEnable=0&MotionDetectionScheduleDay=127&ConfigSystemMotion=Save" >/dev/null 2>&1
+                echo $DATESPECIFIC","$2",SwitchOFF" >> $LOGFILE
                 echo $2" arrived at home since $(date)" | gammu sendsms TEXT $OWNER #Send an SMS to the house owner, thanks to a 3G dongle and gammu program
         fi
 
@@ -29,11 +36,14 @@ do
         then
                 # Do something here
                 COUNTPRESENT=0
-                echo "$(date) - "$2" is away"
+                #echo "$(date) - "$2" is away"
+                echo $DATESPECIFIC","$2",away" >> $LOGFILE
+                curl "http://$CAMIP/setSystemMotion" -H "Host: $CAMIP" -H "Referer: http://$CAMIP/setSystemMotion" -H "Authorization: Basic $CAMAUTHCODE" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" --data "ReplySuccessPage=motion.htm&ReplyErrorPage=motion.htm&MotionDetectionEnable=1&MotionDetectionScheduleDay=127&MotionDetectionScheduleMode=0&MotionDetectionSensitivity=70&ConfigSystemMotion=Save" >/dev/null 2>&1
+                echo $DATESPECIFIC","$2",SwitchOFF" >> $LOGFILE
                 echo $2" is gone since $(date)" | gammu sendsms TEXT $OWNER #Send an SMS to the house owner, thanks to a 3G dongle and gammu program
         fi
 
-    echo "$(date) - confirmation presence: $COUNTPRESENT"
-    echo "$(date) - confirmation absence: $COUNTABSENT"
+    #echo "$(date) - confirmation presence: $COUNTPRESENT"
+    #echo "$(date) - confirmation absence: $COUNTABSENT"
     sleep $3
 done
